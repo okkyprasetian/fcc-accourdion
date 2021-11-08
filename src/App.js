41a05d4248cf70101bf6c8d195ed3f723c8fff57
okkyprasetian <okkyprasetian@gmail.com>
2021-11-08 07:40:33 +0700

    make data.js as state for project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+import data from './data.js'
+
 function App() {
+
+  // State
+  const qA = useState(data)
+
   return (
     <div className="App">
     </div>
--- src/data.js
+++ src/data.js
@@ -2,26 +2,33 @@ const data = [
     {
         id: 1,
         q: 'Who is Yu?',
+        a: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quas id sequi odio eum possimus dignissimos alias ad, repellendus distinctio, quaerat unde ratione. Esse nam nostrum inventore consectetur! Eos unde minus debitis numquam mollitia eveniet et, pariatur quasi reiciendis magnam nam voluptatum! Explicabo quasi, temporibus mollitia assumenda esse magnam ea.',
         buttonC: false
     },
     {
         id: 2,
         q: 'Where is Yu?',
+        a: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quas id sequi odio eum possimus dignissimos alias ad, repellendus distinctio, quaerat unde ratione. Esse nam nostrum inventore consectetur! Eos unde minus debitis numquam mollitia eveniet et, pariatur quasi reiciendis magnam nam voluptatum! Explicabo quasi, temporibus mollitia assumenda esse magnam ea.',
         buttonC: false
     },
     {
         id: 3,
         q: 'When is Yu?',
+        a: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quas id sequi odio eum possimus dignissimos alias ad, repellendus distinctio, quaerat unde ratione. Esse nam nostrum inventore consectetur! Eos unde minus debitis numquam mollitia eveniet et, pariatur quasi reiciendis magnam nam voluptatum! Explicabo quasi, temporibus mollitia assumenda esse magnam ea.',
         buttonC: false
     },
     {
         id: 4,
         q: 'How is Yu?',
+        a: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quas id sequi odio eum possimus dignissimos alias ad, repellendus distinctio, quaerat unde ratione. Esse nam nostrum inventore consectetur! Eos unde minus debitis numquam mollitia eveniet et, pariatur quasi reiciendis magnam nam voluptatum! Explicabo quasi, temporibus mollitia assumenda esse magnam ea.',
         buttonC: false
     },
     {
         id: 5,
         q: 'How Many is Yu?',
+        a: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt quas id sequi odio eum possimus dignissimos alias ad, repellendus distinctio, quaerat unde ratione. Esse nam nostrum inventore consectetur! Eos unde minus debitis numquam mollitia eveniet et, pariatur quasi reiciendis magnam nam voluptatum! Explicabo quasi, temporibus mollitia assumenda esse magnam ea.',
         buttonC: false
     }
 ]
+
+export default data
