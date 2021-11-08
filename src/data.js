2fced2d80ce4fbb1a407448900006e5e53e6107b
okkyprasetian <okkyprasetian@gmail.com>
2021-11-08 07:35:40 +0700

    add data.js

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id: 1,
+        q: 'Who is Yu?',
+        buttonC: false
+    },
+    {
+        id: 2,
+        q: 'Where is Yu?',
+        buttonC: false
+    },
+    {
+        id: 3,
+        q: 'When is Yu?',
+        buttonC: false
+    },
+    {
+        id: 4,
+        q: 'How is Yu?',
+        buttonC: false
+    },
+    {
+        id: 5,
+        q: 'How Many is Yu?',
+        buttonC: false
+    }
+]
